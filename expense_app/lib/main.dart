34b6e7eadeaf356da4e8eab7d17cde5089c975c8
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-04-26 13:28:18 +0530

    Trying alignments with Column widget

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -20,6 +20,8 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -30,6 +32,7 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Card(
+            color: Colors.red,
             child: Text('LIST OF TX'),
           ),
         ],
