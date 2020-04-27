bf3cf9027228d0273159a7534c171af6882e0bf3
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-04-27 21:56:00 +0530

    Custom fonts and text themes

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -17,6 +17,23 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          title: TextStyle(
+            fontFamily: 'OpenSans',
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            title: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )
       ),
       home: MyHomePage(),
     );
