e06a93931ec7fcdd8d10d9d75c3b6037dcb996e6
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-04-28 15:54:40 +0530

    Using ListTile widget

--- expense_app/lib/widgets/transaction_list.dart
+++ expense_app/lib/widgets/transaction_list.dart
@@ -34,45 +34,32 @@ class TransactionList extends StatelessWidget {
         ) :
         ListView.builder(
         itemBuilder: (ctx, index) {
-          return Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10, 
-                  horizontal: 15
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).primaryColor,
-                    width: 2,
-                  )
-                ),
-                padding: EdgeInsets.all(5),
-                child: Text(
-                  '\$${transactions[index].amount.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: Theme.of(context).primaryColor,
+          return Card(
+            elevation: 5,
+            margin: EdgeInsets.symmetric(vertical: 4, horizontal: 3),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                child: Padding(
+                  padding: EdgeInsets.all(6),
+                  child: FittedBox(
+                    child: Text(
+                      '\$${transactions[index].amount.toStringAsFixed(2)}',
+                    ),
                   ),
+                ),
+              ),
+              title: Text(
+                transactions[index].title,
+                style: Theme.of(context).textTheme.title
+              ),
+              subtitle: Text(
+                DateFormat.yMMMd().format(transactions[index].date),
+                style: TextStyle(
+                  color: Colors.grey
                 )
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    transactions[index].title,
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  Text(
-                    DateFormat.yMMMd().format(transactions[index].date),
-                    style: TextStyle(
-                      color: Colors.grey
-                    )
-                  )
-                ],
-              ),
-            ],
+            ),
           );
         },
         itemCount: transactions.length,
