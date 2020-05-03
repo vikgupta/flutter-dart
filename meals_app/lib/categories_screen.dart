7d17310e076817ff98c2e278b87c6412d422ee54
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-03 12:56:05 +0530

    Starting up with grid based design

--- meals_app/lib/categories_screen.dart
+++ meals_app/lib/categories_screen.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 
+import './category_item.dart';
+
+import './dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[
-
-      ],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        children: DUMMY_CATEGORIES.map(
+          (category) => CategoryItem(category.title, category.color)
+        ).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
