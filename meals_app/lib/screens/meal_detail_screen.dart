d83b2696e5cdbc5d7efab1508fc53782135c5dc1
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-04 19:56:24 +0530

    Navigating to the meal details page - continued

---
+++ meals_app/lib/screens/meal_detail_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const String routeName = '/meal-details';
+
+  @override
+  Widget build(BuildContext context) {
+    final meal = ModalRoute.of(context).settings.arguments as Meal;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: Center(
+        child: Text(meal.title),
+      ),
+    );
+  }
+}
