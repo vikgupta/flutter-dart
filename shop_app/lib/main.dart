9e1b438ea4ea0bde14b499eb6bd8f112b6e25611
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-06 18:10:33 +0530

    Managing state using Provider package

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/product_details_screen.dart';
+
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        //home: MyHomePage(),
+        routes: {
+          '/': (ctx) => ProductsOverviewScreen(),
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen()
+        },
       ),
-      //home: MyHomePage(),
-      routes: {
-        '/': (ctx) => ProductsOverviewScreen(),
-        ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen()
-      },
     );
   }
 }
