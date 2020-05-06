7c2998e90e9d1f5e132dfda7881e11e74b281971
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-06 12:04:21 +0530

    Working on ProductItem widget

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screens.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,21 +12,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Shop'),
-      ),
+      //home: MyHomePage(),
+      routes: {
+        '/': (ctx) => ProductsOverviewScreen()
+      },
     );
   }
 }
