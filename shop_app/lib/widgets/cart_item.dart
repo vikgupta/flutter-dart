896a3705e5a44434dd83d144bab3c8f251146862
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-08 16:52:30 +0530

    Showing alert dialog

--- shop_app/lib/widgets/cart_item.dart
+++ shop_app/lib/widgets/cart_item.dart
@@ -53,6 +53,29 @@ class CartItem extends StatelessWidget {
           )
         ),
       ),
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item?'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+              ),
+              FlatButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+              )
+            ],
+          )
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
