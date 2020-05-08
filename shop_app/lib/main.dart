3699e899cc3a65c748e13d5694e7a6af1113a5e7
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-08 20:11:36 +0530

    Implementing Edit Product screen

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -6,6 +6,7 @@ import './screens/product_details_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
 import './providers/products_provider.dart';
 import './providers/cart_provider.dart';
@@ -41,7 +42,8 @@ class MyApp extends StatelessWidget {
           ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
-          UserProductsScreen.routeName: (ctx) => UserProductsScreen()
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
--- shop_app/lib/screens/user_products_screen.dart
+++ shop_app/lib/screens/user_products_screen.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
 
+import '../screens/edit_product_screen.dart';
+
 import '../providers/products_provider.dart';
 
 class UserProductsScreen extends StatelessWidget {
@@ -19,7 +21,9 @@ class UserProductsScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           )
         ],
       ),
