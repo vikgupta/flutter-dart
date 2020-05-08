2132c0932306c43579564a6908b04b2cca818a5d
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-08 12:22:45 +0530

    Adding product detail data

--- shop_app/lib/screens/product_details_screen.dart
+++ shop_app/lib/screens/product_details_screen.dart
@@ -16,10 +16,41 @@ class ProductDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(product.title)
       ),
-      body: Center(
-        child: Text(
-          'DEF'
-        )
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$ ${product.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
