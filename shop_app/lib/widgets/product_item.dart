3f662b7caa3e482995daa9ddbfd67482e5e000c9
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-12 12:36:04 +0530

    Fading loaded images in

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -17,9 +17,10 @@ class ProductItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
         child: GridTile(
           child: GestureDetector(
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/placeholder-product.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
             ),
             onTap: () {
               Navigator.of(context).pushNamed(
