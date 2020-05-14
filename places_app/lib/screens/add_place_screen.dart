9f64e9a14e903523dd3016faeb81d08ee9fa3bde
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-14 13:08:35 +0530

    Using Location package and retrieving the user co-ordinates

--- places_app/lib/screens/add_place_screen.dart
+++ places_app/lib/screens/add_place_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 
 import '../providers/great_places_provider.dart';
 
@@ -54,6 +55,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput(),
                   ],
                 ),
               ),
