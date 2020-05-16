b9b9c78c182a58e0eab1886457ddc89bbe5c0d5a
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-16 23:27:39 +0530

    Setting a firestore trigger

--- chat_app/functions/index.js
+++ chat_app/functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.myFunction = functions.firestore
+  .document('chat/{message}')
+  .onCreate((snapshot, context) => { 
+      console.log(snapshot.data());
+      return;
+   });
