777c67301a78acdb0b75efc1e9cd665df954782b
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-17 21:58:28 +0530

    Running native android call using MethodCall API

--- battery_app/lib/main.dart
+++ battery_app/lib/main.dart
@@ -30,6 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    _getBatteryLevel();
   }
 
   Future<void> _getBatteryLevel() async {
