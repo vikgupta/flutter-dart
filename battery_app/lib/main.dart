fa46e8472f4b6766876e87570f0e68792e0ffd04
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-05-17 08:57:07 +0530

    Boilerplate code for battery_app

---
+++ battery_app/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _batteryLevel;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _getBatteryLevel() async {
+    const platform = MethodChannel('course.flutter.dev/battery');
+    try {
+      final response = await platform.invokeMethod('getBatteryLevel');
+      setState(() {
+        _batteryLevel = response;
+      });
+    } on PlatformException catch(error) {
+      print(error.message);
+      setState(() {
+        _batteryLevel = null;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Native Code'),
+      ),
+      body: Center(
+        child: Text('Battery level - $_batteryLevel'),
+      ),
+    );
+  }
+}
